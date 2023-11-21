4cbc5c550d709e4045774b548d31f1309c6e310c
unknown <shchetinin.srg@yandex.ru>
2023-11-21 10:53:11 +0500

    f

---
+++ main.js
@@ -0,0 +1,7 @@
+function generate () {
+	var active1 = document.getElementById('active').value;
+	document.getElementById('newactive').innerHTML = active1;
+
+	var unactive1 = document.getElementById('unactive').value;
+	document.getElementById('newunactive').innerHTML = unactive1;
+}
